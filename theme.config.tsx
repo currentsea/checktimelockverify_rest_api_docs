4f7441f82172848594c6adba5823dac1a05f9e83
Joe Bull <jtbull@uw.edu>
2023-06-28 00:04:28 +0000

    update
    Signed-off-by: Joe Bull <jtbull@uw.edu>

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Timelock REST API</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/currentsea/checktimelockverify_rest_api_docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/currentsea/checktimelockverify_rest_api_docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'REST API Documentation for creating timelocks',
   },
 }
 
