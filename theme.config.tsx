1cf0b1eb48f6f630404f59f75472bbbecb5a4c34
Joe Bull <joetbull@gmail.com>
2023-06-28 06:06:15 +0000

    update discord link
    Signed-off-by: Joe Bull <joetbull@gmail.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/RQyu7bfv',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
